958d0e6abde3f3c256ed22611365dd0c90eeefb8
tyron052 <ttucker-18@peddie.org>
2016-11-14 12:00:13 -0500

    Add files via upload
    We worked through downloading a dictionary and putting it into a text file for Testa to use his Scanner method. We then created different methods for Testa's use, one being to determine if a word was inside our Dictionary. As well as another of determining if there was another word after the one passed to us.

--- Dictionary.java
+++ Dictionary.java
@@ -1,65 +1,104 @@
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Dictionary 
 {
-	/*public static void main(String[] args) throws FileNotFoundException
+	public static void main(String[] args) throws FileNotFoundException //tester code 
 	{
-		ArrayList<String>rawData = loadStrings("Boggle.txt");
-		String filename = "Boggle.txt";
-		Scanner scan = new Scanner (new File (filename)).useDelimiter("\\\\");
+		Scanner scan = new Scanner (new File ("Boggle.txt")); //scanning the dictionary that's in a text file
 		String word;
-		Scanner inputs = new Scanner(System.in);
-
-		ArrayList<String> wordList = new ArrayList<String>();
+		Scanner inputs = new Scanner(System.in);//scanner for testing 
 		
-		while (scan.hasNext()) 
+		ArrayList<String> wordList = new ArrayList<String>();//
+		
+		while (scan.hasNext()) //looping through the scanner and adding each word to the next line of the created ArrayList
 		{
             word = scan.next();
-            wordList.add(word);
-            
+            wordList.add(word.substring(0, word.length()-1));
+                        
         }
 		
-		for(int i = 0; i<rawData.size(); i++)
+		for(int i = 0; i < wordList.size(); i++)//printing the list out
 		{
 			System.out.println(wordList.get(i));
 		}
-		for(int i = 0; i < rawData.size(); i++)
+		
+		for(int i = 0; i < wordList.size(); i++)//Checks to see if the passsed string is in the dictionary 
 		{
 			System.out.print("Enter a word ");
 			String a = inputs.nextLine();
-			a+="\\";
-			if(rawData.contains(a))
+			
+			if(wordList.contains(a))
 				System.out.println("Found");
 			else
 				System.out.println("Does not exist");
 		}
 		
 	}
-	*/
-	private ArrayList<String>rawData = new ArrayList<String>();
 	
-	public Dictionary(String filename)
+	private ArrayList<String>wordList = new ArrayList<String>();
+	private Scanner scan;
+	private String b;
+	
+	public Dictionary()//default constructor 
 	{
-		rawData = loadStrings(filename);
-	}
-	public boolean isWord(String s)
-	{
-		for(int i = 0; i < rawData.size(); i++)
+		try 
 		{
-			s+="\\";
-			if(rawData.contains(s))
-				return true;
+			scan = new Scanner (new File ("Boggle.txt"));
+		} catch (FileNotFoundException e) 
+		{
+			e.printStackTrace();
 		}
+		editWord();
+	}
+	
+	public boolean isWord(String s)//tells if the passed string is a word or not
+	{
+		b = s;
+		b = b.toLowerCase();
+		if(wordList.contains(b))
+			return true;
+		
 		return false;
 		
 	}
-	private static ArrayList<String> loadStrings(String fileName) 
+	
+	private void editWord()//edits the word passed to solve our backslash issue.
+	{
+		String word;
+		while (scan.hasNext()) 
+		{
+            word = scan.next();
+            wordList.add(word.substring(0, word.length()-1));         
+        }
+		
+	}
+	
+	public String getFirst()//Gets the first word in the dictionary
+	{
+		return wordList.get(0);
+	}
+	
+	public String getNext()//gets the next word in the dictionary after one has been passed. If no word is found after, then the loop ends.	
+	{
+		String next;
+		int index;
+		index = wordList.indexOf(b);
+		next = wordList.get(index+1);
+		return next;
+	}
+	
+	public boolean hasNext(String s)//checks to see if there is another word after the passed word.
+	{
+		b.toLowerCase();
+		if(scan.hasNext(b))
+			return true; 
+		return false;
+	}
+	
+	/*private static ArrayList<String> loadStrings(String fileName) 
 	{
 		String line = "";
 		ArrayList<String> toReturn = new ArrayList<String>();
@@ -78,6 +117,7 @@ public class Dictionary
 		}
 		return toReturn;
 	}
+	*/
 	
 	    
 	 
