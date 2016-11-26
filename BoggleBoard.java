9d107344d0cee0f1c2984e652a1f953db79a7c3f
Winston Yang <wyang737@users.noreply.github.com>
2016-11-26 11:55:25 -0500

    Updated Onboard feature

--- BoggleBoard.java
+++ BoggleBoard.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 public class BoggleBoard {
 
 	public BoggleBoard() {
-		makeBoard(); // added this
+		makeBoard();
 	}
 
 	char[][] board = new char[4][4];
 
 	public void makeBoard() {
 		char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
-		Random x = new Random(vowels.length);
+		Random x = new Random();
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[i].length; j++) {
 				if (x.nextInt(26) % 3 == 0) {
@@ -33,24 +33,45 @@ public class BoggleBoard {
 		}
 	}
 
-	// basic idea:
+	boolean[][] visited = new boolean[4][4]; 
 	
-	String find = ""; // search word
-	boolean[][] b = new boolean[4][4]; // mark where we have been, where we haven't been
-	
-	public boolean onBoard(int i, int j, String s) { // call method in a nested for loop
-		b[i][j] = true; // mark location
-		find += board[i][j]; // add character to search word
-		for (int row = i - 1; row <= i + 1 && row < board.length; row++) { // go to adjacent squares
-			for (int col = j - 1; col <= j + 1 && col < board[0].length; col++) {
-				if (row >= 0 && col >= 0 && b[row][col] == false) { // prevent array out of bounds, make sure we haven't been there yet
-					onBoard(row, col, s);
+	public boolean onBoard(BogglePoint point, char[] word, int pos) { 
+		if (board[point.x][point.y] != word[pos]){
+			return false;
+		}
+		if (word.length - 1 == pos){
+			return true;
+		}
+		visited[point.x][point.y] = true;
+		pos ++;
+		BogglePoint[] points = getAdjacent(point);
+		for (int i = 0; i < points.length; i ++){
+			BogglePoint nextPoint = points[i];
+			if (nextPoint != null){
+				if (!visited[nextPoint.x][nextPoint.y] && onBoard(nextPoint, word, pos)){
+					return true;
 				}
 			}
 		}
-		// if everything fails: 
-		find = find.substring(0, find.length() - 1); // erase last letter of search word to reverse the action above
-		b[i][j] = false; // mark the spot as unvisited
+		visited[point.x][point.y] = false;
 		return false;
+	}
+	
+	public BogglePoint[] getAdjacent(BogglePoint point){
+		BogglePoint[] adjacent = new BogglePoint[8];
+		for (int i = 0; i < adjacent.length; i ++){
+			adjacent[i] = null;
+		}
+		int counter = -1;
+		for (int i = -1; i <= 1; i ++){
+			for (int j = -1; j <= 1; j ++){
+				BogglePoint point2 = new BogglePoint(point.x + i, point.y + j);
+				if (point2.x >= 0 && point2.x <= 3 && point2.y >= 0 && point2.y <= 3 && (i != 0 || j != 0)){
+					counter ++;
+					adjacent[counter] = point2;
+				}
+			}
+		}
+		return adjacent;
 	}
 }
